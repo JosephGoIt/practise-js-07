7188f53f6e159066e39173715b2a1652ecd0bb0d
Joseph Camacho <spongkj@gmail.com>
2024-02-10 21:55:37 +0800

    test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,32 +5,30 @@ import * as basicLightbox from 'basiclightbox';
 const gallery = document.querySelector('.gallery');
 
 galleryItems.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('gallery__item');
-  
-    listItem.innerHTML = `
-      <a class="gallery__link" href="${item.original}">
-        <img
-          class="gallery__image"
-          src="${item.preview}"
-          data-source="${item.original}"
-          alt="${item.description}"
-        />
-      </a>
-    `;
-  
-    gallery.appendChild(listItem);
-  });
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery__item');
+
+  listItem.innerHTML = `
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  `;
+
+  gallery.appendChild(listItem);
+});
 
 // Event delegation to open modal
 gallery.addEventListener('click', event => {
-  event.preventDefault(); // Prevent default behavior of anchor tag
+    event.preventDefault(); // Prevent default behavior of anchor tag
 
-  if (event.target.classList.contains('gallery__image')) {
-    const imageUrl = event.target.dataset.source;
-    const instance = basicLightbox.create(`<img src="${imageUrl}" alt="Image">`);
-    instance.show();
-  }
-});
-
-console.log(galleryItems);
+    if (event.target.classList.contains('gallery__image')) {
+      const imageUrl = event.target.dataset.source;
+      const instance = basicLightbox.create(`<img src="${imageUrl}" alt="Image">`);
+      instance.show();
+    }
+  });
